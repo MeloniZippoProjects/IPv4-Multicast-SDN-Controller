4d88e77bd1c041479b0220b207856140781e7e02
enricomeloni <enrico-meloni@hotmail.it>
2018-04-11 14:52:02 +0200

    Add default gw to hosts

--- simulation/sim.py
+++ simulation/sim.py
@@ -4,6 +4,8 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
+from mininet.cli import CLI
+
 
 class SingleSwitchTopo(Topo):
     "Single switch connected to n hosts."
@@ -12,17 +14,19 @@ class SingleSwitchTopo(Topo):
         # Python's range(N) generates 0..N-1
         for h in range(n):
             host = self.addHost('h%s' % (h + 1))
+            host.cmd("route add default gw 10.0.0.100")
             self.addLink(host, switch)
 
 def simpleTest():
     "Create and test a simple network"
-    topo = SingleSwitchTopo(n=4)
+    topo = SingleSwitchTopo(n=3)
     net = Mininet(topo)
     net.start()
     print("Dumping host connections")
     dumpNodeConnections(net.hosts)
     print("Testing network connectivity")
     net.pingAll()
+    CLI(net)
     net.stop()
 
 if __name__ == '__main__':
