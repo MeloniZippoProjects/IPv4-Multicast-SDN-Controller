981aa17a4c26ac85035e5049d487a5fb13a7162e
Raffaele Zippo <raffaele.zippo@outlook.com>
2018-04-11 15:16:58 +0200

    Add default gw to hosts, add cmd line parameters

--- simulation/sim.py
+++ simulation/sim.py
@@ -7,7 +7,7 @@ from mininet.log import setLogLevel
 from mininet.node import RemoteController
 from functools import partial
 from mininet.cli import CLI
-
+import sys
 class SingleSwitchTopo(Topo):
     "Single switch connected to n hosts."
     def build(self, n=2):
@@ -15,22 +15,30 @@ class SingleSwitchTopo(Topo):
         # Python's range(N) generates 0..N-1
         for h in range(n):
             host = self.addHost('h%s' % (h + 1))
-            host.cmd("route add default gw 10.0.0.100")
             self.addLink(host, switch)
 
-def simpleTest():
+def createNetwork():
     "Create and test a simple network"
     topo = SingleSwitchTopo(n=3)
-    net = Mininet(topo=topo, controller=partial( RemoteController, ip='192.168.100.93', port=6653))
+    net = Mininet(topo=topo, controller=partial( RemoteController, ip=sys.argv[1], port=6653))
     net.start()
     print("Dumping host connections")
     dumpNodeConnections(net.hosts)
+
+    print("Adding default gw to hosts")
+    for host in net.hosts:
+        host.cmd("route add default gw " + sys.argv[2])
+        print(" - " + host.name + ": DONE")
     print("Testing network connectivity")
     net.pingAll()
     CLI(net)
     net.stop()
 
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: ./mnStart.py {controller address} {default gateway address}")
+        sys.exit()
+    
     # Tell mininet to print useful information
     setLogLevel('info')
-    simpleTest()
+    createNetwork()
