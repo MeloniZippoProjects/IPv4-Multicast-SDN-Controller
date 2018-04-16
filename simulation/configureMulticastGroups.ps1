b0ec607cfd26440598dcf0bbafac77523e1fba8b
enricomeloni <enrico-meloni@hotmail.it>
2018-04-16 15:36:39 +0200

    Generalize Join-Host in powershell script

--- simulation/configureMulticastGroups.ps1
+++ simulation/configureMulticastGroups.ps1
@@ -15,15 +15,25 @@ $restAPI = "http://192.168.100.93:8080/";
 
 function Call-Api($action, $method, $body)
 {
-    Write-Host $method
     $response = Invoke-WebRequest -Uri ($restAPI + $action) -Method $method -Body $body -ContentType "application/json"
     $response
 }
 
+function Join-Host($lastByte)
+{
+    $joinHost = @{
+        "host" = "10.0.0."+$lastByte;
+    } | ConvertTo-Json
+
+    $joinHost
+}
+
 
 Call-Api "multicastgroups/" 'Put' $createGroup
-Call-Api "multicastgroups/1/hosts" 'Put' $joinHost1
-Call-Api "multicastgroups/1/hosts" 'Put' $joinHost2
+Call-Api "multicastgroups/1/hosts" 'Put' (Join-Host 2)
+Call-Api "multicastgroups/1/hosts" 'Put' (Join-Host 4)
+Call-Api "multicastgroups/1/hosts" 'Put' (Join-Host 7)
+Call-Api "multicastgroups/1/hosts" 'Put' (Join-Host 8)
 Call-Api "multicastgroups/" 'Get'
 
 
